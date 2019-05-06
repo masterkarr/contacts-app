afe28e7c10c714171ddecdea7b5dddd002263541
Josh Karr <karr.josh@gmail.com>
2019-05-05 23:39:18 -0600

    application to register users and update a profile

---
+++ Csharp_Contacts_App/Csharp_Contacts_App/Globals.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Contacts_App
+{
+    class Globals
+    {
+        // class to make the logged in user id global everywhere on the application
+        
+
+        public static int GlobalUserId { get; private set; }
+
+        public static void SetGlobalUserIId(int userId)
+        {
+            GlobalUserId = userId;
+        }
+
+    }
+}
